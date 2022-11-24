b575f4fd60f7772ab87b2757e8e05c960c53502f
imtiazraqib <imtiazud@ualberta.ca>
2022-11-24 12:19:17 -0700

    set up basic graphql scehma and resolver

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { graphqlHTTP }  = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-    res.send('Hello World!')
-});
+// Setting up GraphQL Scehmas and Resolvers
+app.use('/graphql', graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+        type RootMutation {
+            creatEvent(name: String): String
+        }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['Romantic Cooking', 'Soccer Practice', 'Coding'];
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
+}));
 
 app.listen(3000);
