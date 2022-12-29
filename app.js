8a94476af0687ceb8bbec3d3c930a6ff88cea678
imtiazraqib <imtiazud@ualberta.ca>
2022-12-28 17:10:07 -0700

    connected to mongodb

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlHTTP }  = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+
+
 
 const app = express();
 
-const events = [];
+const Event = require('./models/events');
 
 app.use(bodyParser.json());
 
@@ -42,22 +45,38 @@ app.use(
         `),
         rootValue: {
             events: () => {
-                return events;
+                return Event.find().then(events => {
+                    return events.map(event => {
+                        return {...event._doc};
+                    })
+                }).catch(err => {
+                    throw err;
+                })
             },
             createEvent: args => {
-                const event = {
-                    _id: Math.random().toString(),
+                const event = new Event({
                     title: args.eventInput.title,
                     description: args.eventInput.description,
                     price: +args.eventInput.price,
-                    date: args.eventInput.date
-                };
-                events.push(event);
-                return event;
+                    date: new Date(args.eventInput.date)
+                });
+                return event.save().then(result => {
+                    return {...result._doc};
+                }).catch(err => {
+                    console.log(err);
+                    throw err;
+                });
             }
         },
         graphiql: true
     })
 );
 
-app.listen(3000);
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.kx3gjgj.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
+.then(() => {
+    app.listen(3000);
+})
+.catch(err => {
+    console.log(err);
+})
+
